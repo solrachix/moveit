b10a22f990183414f2f9cd1fb8284c55058cf009
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2021-02-23 14:08:53 -0300

    adding: CompletedChallenges component

---
+++ src/components/CompletedChallenges/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles'
+
+const CompletedChallenges = () => {
+  return (
+    <Container className="completedChallengesContainer">
+      <span>Desafios completos</span>
+      <span>05</span>
+    </Container>
+  )
+}
+
+export default CompletedChallenges
